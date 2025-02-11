7c48f09413a10307cc0ffdd942fed8c9e3308596
Tanishq <146183279+TanishqGaur20@users.noreply.github.com>
2025-02-11 17:55:21 +0530

    changes for uI

--- backend/routes/commentRoutes.js
+++
@@ -1,6 +0,0 @@
-import { getComment, addComment } from "../controllers/commentController.js";
-
-export function commentRoutes(app) {
-  app.get("/comment", getComment);
-  app.post("/comment", addComment);
-}
